be72b6219f7c6af6a59d2d50be0a8db7f70b1129
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-09-02 19:17:55 +0900

    design: login ui 수정

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -72,7 +72,7 @@ export function SignIn() {
                 <div className="text-sm">
                   <a
                     href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    className="font-semibold text-blue-600 hover:text-blue-500"
                   >
                     Forgot password?
                   </a>
@@ -96,7 +96,7 @@ export function SignIn() {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex w-full justify-center rounded-md bg-blue-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
               >
                 로그인
               </button>
@@ -106,8 +106,8 @@ export function SignIn() {
           <div className="mt-6 text-center">
             아직 계정이 없으신가요?{" "}
             <Link
-              to="./SignUp"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+              to="/SignUp"
+              className="font-semibold leading-6 text-blue-600 hover:text-blue-500"
             >
               회원가입하기
             </Link>
