7d111cc8588c8d310f49f308760932b25579100c
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-09-02 19:57:03 +0900

    design: SignUp page UI 수정

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -2,183 +2,128 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import icon from "../assets/pill.png";
 
-export function SignUp() {
-  const [name, setName] = useState("");
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirm, setPasswordConfirm] = useState("");
-  const [birthday, setBirthday] = useState("");
+function SignUp() {
+  const [formData, setFormData] = useState({
+    name: "",
+    id: "",
+    password: "",
+    passwordConfirm: "",
+    birthday: "",
+    userType: "",
+  });
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
-  const handleIdChange = (event) => {
-    setId(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handlePasswordConfirmChange = (event) => {
-    setPasswordConfirm(event.target.value);
-  };
-
-  const handleBirthdayChange = (event) => {
-    setBirthday(event.target.value);
+  const handleUserTypeChange = (selectedType) => {
+    setFormData({
+      ...formData,
+      userType: selectedType,
+    });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    console.log("Name:", name);
-    console.log("ID:", id);
-    console.log("Password:", password);
-    console.log("Password Confirmation:", passwordConfirm);
-    console.log("Birthday:", birthday);
-
-    // 여기에서 회원 가입 처리를 수행하거나 필요한 유효성 검사를 추가할 수 있습니다.
+    console.log("Form Data:", formData);
   };
 
   return (
-    <>
-      <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img className="mx-auto h-10 w-auto" src={icon} alt="MMS" />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign Up
-          </h2>
-        </div>
+    <div className="flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img className="mx-auto h-10 w-auto" src={icon} alt="MMS" />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign Up
+        </h2>
+      </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <div>
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Name
-              </label>
-              <div className="mt-2">
-                <input
-                  id="name"
-                  name="name"
-                  type="text"
-                  autoComplete="name"
-                  value={name}
-                  onChange={handleNameChange}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="id"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                ID
-              </label>
-              <div className="mt-2">
-                <input
-                  id="id"
-                  name="id"
-                  type="text"
-                  autoComplete="username"
-                  value={id}
-                  onChange={handleIdChange}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Password
-              </label>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="new-password"
-                  value={password}
-                  onChange={handlePasswordChange}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="passwordConfirm"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Confirm Password
-              </label>
-              <div className="mt-2">
-                <input
-                  id="passwordConfirm"
-                  name="passwordConfirm"
-                  type="password"
-                  autoComplete="new-password"
-                  value={passwordConfirm}
-                  onChange={handlePasswordConfirmChange}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="birthday"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Birthday
-              </label>
-              <div className="mt-2">
-                <input
-                  id="birthday"
-                  name="birthday"
-                  type="date"
-                  value={birthday}
-                  onChange={handleBirthdayChange}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-blue-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-              >
-                Sign Up
-              </button>
-            </div>
-          </form>
-
-          <div className="mt-6 text-center">
-            이미 계정이 있으신가요?{" "}
-            <Link
-              to="/"
-              className="font-semibold leading-6 text-blue-600 hover:text-blue-500"
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <div className="flex items-center justify-center space-x-4">
+            <button
+              type="button"
+              onClick={() => handleUserTypeChange("medical")}
+              className={`${
+                formData.userType === "medical"
+                  ? "bg-blue-400 text-white"
+                  : "bg-gray-200 text-gray-800"
+              } py-1.5 px-4 rounded-md text-sm font-semibold`}
             >
-              로그인하기
-            </Link>
+              의료인
+            </button>
+            <button
+              type="button"
+              onClick={() => handleUserTypeChange("patient")}
+              className={`${
+                formData.userType === "patient"
+                  ? "bg-blue-400 text-white"
+                  : "bg-gray-200 text-gray-800"
+              } py-1.5 px-4 rounded-md text-sm font-semibold`}
+            >
+              환자
+            </button>
           </div>
+
+          {["name", "id", "password", "passwordConfirm", "birthday"].map(
+            (fieldName) => (
+              <div key={fieldName}>
+                <label
+                  htmlFor={fieldName}
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  {fieldName === "passwordConfirm"
+                    ? "Confirm Password"
+                    : fieldName.charAt(0).toUpperCase() + fieldName.slice(1)}
+                </label>
+                <div className="mt-2">
+                  <input
+                    id={fieldName}
+                    name={fieldName}
+                    type={
+                      fieldName === "birthday"
+                        ? "date"
+                        : fieldName === "password" ||
+                          fieldName === "passwordConfirm"
+                        ? "password"
+                        : "text"
+                    }
+                    autoComplete={fieldName}
+                    value={formData[fieldName]}
+                    onChange={handleInputChange}
+                    required
+                    style={{ paddingLeft: "10px" }}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+            )
+          )}
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-blue-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+
+        <div className="mt-6 text-center">
+          이미 계정이 있으신가요?{" "}
+          <Link
+            to="/"
+            className="font-semibold leading-6 text-blue-600 hover:text-blue-500"
+          >
+            로그인하기
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignIn from "./components/SignIn.jsx";
 import SignUp from "./components/SignUp.jsx";
 import "./index.css";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
@@ -13,6 +14,5 @@ ReactDOM.render(
         <Route path="/SignUp" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
