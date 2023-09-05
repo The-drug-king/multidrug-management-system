a75a5dfdb1705613bc29c6c4b2897370d8046f22
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-09-05 19:40:18 +0900

    feat: Header Login / Logout 기능 셋팅

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "../styles/Header.css";
 
 const Header = () => {
@@ -8,12 +9,44 @@ const Header = () => {
     setIsLoggedIn(!isLoggedIn);
   };
 
+  const handleLogout = async () => {
+    // 로그아웃 요청을 보낼 API 엔드포인트 URL
+    const logoutApiUrl = "API_LOGOUT_URL_HERE"; // 실제 로그아웃 API URL로 변경
+
+    const headers = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+
+    try {
+      const response = await fetch(logoutApiUrl, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify({}),
+      });
+
+      if (response.ok) {
+        setIsLoggedIn(false);
+      } else {
+        console.error("로그아웃 실패:", response.status);
+        alert("로그아웃 중 문제가 발생했습니다.");
+      }
+    } catch (error) {
+      console.error("로그아웃 중 오류 발생:", error);
+      alert("로그아웃 중 오류가 발생했습니다.");
+    }
+  };
+
   return (
     <div className="header-container">
       <div className="logo">MMS</div>
 
-      <div className="user" onClick={handleLoginToggle}>
-        {isLoggedIn ? <> 로그아웃</> : <>로그인</>}
+      <div className="user">
+        {isLoggedIn ? (
+          <button onClick={handleLogout}>로그아웃</button>
+        ) : (
+          <Link to="/SignIn">로그인</Link>
+        )}
       </div>
     </div>
   );
