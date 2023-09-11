b5a7c31d8568e26fd5a8565fe17c331d6e4678eb
dung_dung_e <103167624+ella00100@users.noreply.github.com>
2023-09-11 09:17:53 +0900

    design: addPatientModal design 수정

--- frontend/src/components/AddPatientModal.jsx
+++ frontend/src/components/AddPatientModal.jsx
@@ -73,18 +73,19 @@ const AddPatientModal = ({ isOpen, onClose, onAddPatient, newPatientId }) => {
               <div className="bg-gray-50 px-4 py-3 flex sm:flex-row-reverse sm:px-6">
                 <button
                   type="button"
-                  className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                  className="mt-3  w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-700 sm:ml-3 sm:w-auto"
+                  onClick={handleSubmit}
+                >
+                  추가
+                </button>
+
+                <button
+                  type="button"
+                  className="mt-3  w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-100 sm:ml-0 sm:w-auto"
                   onClick={onClose}
                   ref={cancelButtonRef}
                 >
                   취소
-                </button>
-                <button
-                  type="button"
-                  className="mt-3 inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-700 sm:ml-3 sm:w-auto"
-                  onClick={handleSubmit}
-                >
-                  추가
                 </button>
               </div>
             </div>
